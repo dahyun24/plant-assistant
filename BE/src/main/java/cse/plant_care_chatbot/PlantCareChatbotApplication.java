16749443d792bcfacb243516eee837b3f561a186
다현 <123882512+dahyun24@users.noreply.github.com>
2025-11-20 02:22:16 +0900

    chore: 프로젝트 초기 설정

---
+++ BE/src/main/java/cse/plant_care_chatbot/PlantCareChatbotApplication.java
@@ -0,0 +1,13 @@
+package cse.plant_care_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantCareChatbotApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/cse/plant_care_chatbot/PlantCareChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package cse.plant_care_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCareChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
