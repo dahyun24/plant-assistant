ed8230849e1470e948a52d34e7a17995514897e8
다현 <123882512+dahyun24@users.noreply.github.com>
2025-12-02 21:36:34 +0900

    fix: class에서 record로 변경

--- BE/src/main/java/cse/plant_care_chatbot/plant/dto/GeminiAnalysisResult.java
+++ BE/src/main/java/cse/plant_care_chatbot/plant/dto/GeminiAnalysisResult.java
@@ -1,16 +1,7 @@
 package cse.plant_care_chatbot.plant.dto;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class GeminiAnalysisResult {
-    private String plantName;   // 추론된 식물 이름 (예: 보스턴고사리)
-    private String growthLevel; // 추론된 성장 단계 (High, Medium, Low, Die)
-    private String caption;     // 이미지 캡션 (건강 상태 묘사)
-}
+public record GeminiAnalysisResult (
+    String plantName,   // 추론된 식물 이름 (예: 보스턴고사리)
+    String growthLevel, // 추론된 성장 단계 (High, Medium, Low, Die)
+    String caption     // 이미지 캡션 (건강 상태 묘사)
+){}
